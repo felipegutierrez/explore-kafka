7534effa3fa8acd4c4f0d541c21cd00bffd6a7dd
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-07-27 16:04:29 +0200

    create first async producer

--- src/main/java/com/github/felipegutierrez/kafka/app/App.java
+++ src/main/java/com/github/felipegutierrez/kafka/app/App.java
@@ -1,7 +1,10 @@
 package com.github.felipegutierrez.kafka.app;
 
+import com.github.felipegutierrez.kafka.producers.ProducerStringAsync;
+
 public class App {
     public static void main(String[] args) {
         System.out.printf("Exploring Kafka");
+        new ProducerStringAsync();
     }
 }

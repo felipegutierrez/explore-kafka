0e5b1a3d7669184ce655f69d3b5edd9d16785aa7
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-07-28 15:18:08 +0200

    add ENABLE_IDEMPOTENCE_CONFIG = true to the Twitter demo example

--- src/main/java/com/github/felipegutierrez/kafka/producers/TwitterProducer.java
+++ src/main/java/com/github/felipegutierrez/kafka/producers/TwitterProducer.java
@@ -129,6 +129,7 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, Boolean.TRUE.toString());
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
         return producer;

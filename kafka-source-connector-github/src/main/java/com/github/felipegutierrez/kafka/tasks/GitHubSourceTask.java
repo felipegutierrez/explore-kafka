12da2837d1e7a696dfd08c23496d4d6f8cf8572e
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-07-31 10:28:54 +0200

    create POJO's to model the github rest API with unit tests for the Kafka source github connector

--- kafka-source-connector-github/src/main/java/com/github/felipegutierrez/kafka/tasks/GitHubSourceTask.java
+++ kafka-source-connector-github/src/main/java/com/github/felipegutierrez/kafka/tasks/GitHubSourceTask.java
@@ -1,6 +1,9 @@
 package com.github.felipegutierrez.kafka.tasks;
 
 import com.github.felipegutierrez.kafka.config.GitHubSourceConnectorConfig;
+import com.github.felipegutierrez.kafka.model.Issue;
+import com.github.felipegutierrez.kafka.model.PullRequest;
+import com.github.felipegutierrez.kafka.model.User;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
@@ -11,6 +14,8 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 import java.util.*;
+
+import static com.github.felipegutierrez.kafka.schemas.GitHubSchemas.*;
 
 public class GitHubSourceTask extends SourceTask {
     private static final Logger log = LoggerFactory.getLogger(GitHubSourceTask.class);
@@ -41,5 +46,35 @@ public class GitHubSourceTask extends SourceTask {
     @Override
     public void stop() {
 
+    }
+
+    public Struct buildRecordValue(Issue issue){
+
+        // Issue top level fields
+        Struct valueStruct = new Struct(VALUE_SCHEMA)
+                .put(URL_FIELD, issue.getUrl())
+                .put(TITLE_FIELD, issue.getTitle())
+                .put(CREATED_AT_FIELD, Date.from(issue.getCreatedAt()))
+                .put(UPDATED_AT_FIELD, Date.from(issue.getUpdatedAt()))
+                .put(NUMBER_FIELD, issue.getNumber())
+                .put(STATE_FIELD, issue.getState());
+
+        // User is mandatory
+        User user = issue.getUser();
+        Struct userStruct = new Struct(USER_SCHEMA)
+                .put(USER_URL_FIELD, user.getUrl())
+                .put(USER_ID_FIELD, user.getId())
+                .put(USER_LOGIN_FIELD, user.getLogin());
+        valueStruct.put(USER_FIELD, userStruct);
+
+        // Pull request is optional
+        PullRequest pullRequest = issue.getPullRequest();
+        if (pullRequest != null) {
+            Struct prStruct = new Struct(PR_SCHEMA)
+                    .put(PR_URL_FIELD, pullRequest.getUrl())
+                    .put(PR_HTML_URL_FIELD, pullRequest.getHtmlUrl());
+            valueStruct.put(PR_FIELD, prStruct);
+        }
+        return valueStruct;
     }
 }

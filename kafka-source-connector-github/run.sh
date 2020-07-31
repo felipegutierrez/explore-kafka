e600cab2a991d73e66d1ad3ab829d2e89a9c8117
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-07-31 13:54:05 +0200

    test Kafka Connect to retrieve github issues

---
+++ kafka-source-connector-github/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# make sure that you execute "mvn clean package" first to create the jar file
+
+# export the path
+export CLASSPATH="$(find target/ -type f -name '*.jar'| grep '\-package' | tr '\n' ':')"
+if hash docker 2>/dev/null; then
+    # for docker lovers
+    sudo docker build . -t explore-kafka/kafka-source-connector-github:1.0
+    sudo docker run --net=host --rm -t \
+           -v $(pwd)/offsets:/kafka-source-connector-github/offsets \
+           explore-kafka/kafka-source-connector-github:1.0
+elif hash connect-standalone 2>/dev/null; then
+    # for mac users who used homebrew
+    connect-standalone config/worker.properties config/GitHubSourceConnectorExample.properties
+elif [[ -z $KAFKA_HOME ]]; then
+    # for people who installed kafka vanilla
+    $KAFKA_HOME/bin/connect-standalone.sh $KAFKA_HOME/etc/schema-registry/connect-avro-standalone.properties config/GitHubSourceConnectorExample.properties
+elif [[ -z $CONFLUENT_HOME ]]; then
+    # for people who installed kafka confluent flavour
+    $CONFLUENT_HOME/bin/connect-standalone $CONFLUENT_HOME/etc/schema-registry/connect-avro-standalone.properties config/GitHubSourceConnectorExample.properties
+else
+    printf "Couldn't find a suitable way to run kafka connect for you.\n \
+Please install Docker, or download the kafka binaries and set the variable KAFKA_HOME."
+fi;

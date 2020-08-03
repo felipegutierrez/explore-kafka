aa2c682d4c123b317409932e17a3155a6dc0e764
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-08-03 10:10:14 +0200

    running Kafka source connector to GitHub on docker image successfully =).

--- kafka-source-connector-github/README.md
+++ kafka-source-connector-github/README.md
@@ -7,13 +7,26 @@ This code is based on the repository [https://github.com/simplesteph/kafka-conne
 ```
 mvn clean package
 sudo docker run -it --rm -p 2181:2181 -p 3030:3030 -p 8081:8081 -p 8082:8082 -p 8083:8083 -p 9092:9092 -e ADV_HOST=127.0.0.1 -e RUNTESTS=0 \
-    -v /home/felipe/workspace-idea/explore-kafka/kafka-source-connector-github/target/kafka-source-connector-github-1.0-with-dependencies.jar:/connectors/GitHub \
-    landoop/fast-data-dev
+    -v /home/felipe/workspace-idea/explore-kafka/kafka-source-connector-github/target/kafka-source-connector-github-1.0:/connectors/GitHub \
+    lensesio/fast-data-dev:2.3.0
 ```
 
 Access the Landoop Kafka Development Environment at [http://127.0.0.1:3030/](http://127.0.0.1:3030/). Access the log service file [http://127.0.0.1:3030/logs/connect-distributed.log](http://127.0.0.1:3030/logs/connect-distributed.log) for errors. You should see one connector alive as it is shown on the figure below.
 
 
 ![Kafka Development Environment](figures/connector-alive.png)
+
+![GitHubSourceConnector available](figures/github-source-connector.png)
+
+Then configure the new github source connector.
+
+![new github source connector configuration](figures/new-github-connector-kubernetes.png)
+
+![new github source connector created](figures/new-github-connector-kubernetes-created.png)
+
+The `github-issues` topic is available and consumming data.
+
+![GitHub issues topic available](figures/github-issues-topic.png)
+
 
 
--- kafka-source-connector-github/run.sh
+++ kafka-source-connector-github/run.sh
@@ -3,20 +3,26 @@
 # make sure that you execute "mvn clean package" first to create the jar file
 
 # export the path
-export CLASSPATH="$(find target/ -type f -name '*with-dependencies.jar'| grep '\-package' | tr '\n' ':')"
+# export CLASSPATH="$(find target/kafka-source-connector-github-1.0/ -type f -name '*.jar'| grep '\-package' | tr '\n' ':')"
+export CLASSPATH="$(find target/ -type f -name '*.jar' | tr '\n' ':')"
+echo $CLASSPATH
 if hash docker 2>/dev/null; then
+    echo "1"
     # for docker lovers
     sudo docker build . -t explore-kafka/kafka-source-connector-github:1.0
     sudo docker run --net=host --rm -t \
            -v $(pwd)/offsets:/kafka-source-connector-github/offsets \
            explore-kafka/kafka-source-connector-github:1.0
 elif hash connect-standalone 2>/dev/null; then
+    echo "2"
     # for mac users who used homebrew
     connect-standalone config/worker.properties config/GitHubSourceConnectorExample.properties
 elif [[ -z $KAFKA_HOME ]]; then
+    echo "3"
     # for people who installed kafka vanilla
     $KAFKA_HOME/bin/connect-standalone.sh $KAFKA_HOME/etc/schema-registry/connect-avro-standalone.properties config/GitHubSourceConnectorExample.properties
 elif [[ -z $CONFLUENT_HOME ]]; then
+    echo "4"
     # for people who installed kafka confluent flavour
     $CONFLUENT_HOME/bin/connect-standalone $CONFLUENT_HOME/etc/schema-registry/connect-avro-standalone.properties config/GitHubSourceConnectorExample.properties
 else

c63b16bc9e15f8d2f8a31b88e61f3bf354fbb9fe
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-08-04 17:11:14 +0200

    add useful command

--- kafka-rest-proxy/src/main/resources/0-create-topics.sh
+++ kafka-rest-proxy/src/main/resources/0-create-topics.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+echo "start the docker-compose with Kafka"
+echo "sudo docker-compose up"
+
+echo "create the topics"
 kafka-topics --create --zookeeper localhost:2181 --topic rest-binary --replication-factor 1 --partitions 1
 kafka-topics --create --zookeeper localhost:2181 --topic rest-json --replication-factor 1 --partitions 1
 kafka-topics --create --zookeeper localhost:2181 --topic rest-avro --replication-factor 1 --partitions 1
+
+echo "keep consuming from CLI"
+echo "kafka-avro-console-consumer --topic rest-avro --bootstrap-server 127.0.0.1:9092 --property schema.registry.url=http://127.0.0.1:8081 --from-beginning"
+

78e38dd513d5e4a427e46b7e36d3d487252a2cd6
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-04-27 15:22:03 +0200

    using Lombok to eliminate boilerplate code

--- kafka-source-connector-github/src/main/java/com/github/felipegutierrez/kafka/connector/model/Label.java
+++ kafka-source-connector-github/src/main/java/com/github/felipegutierrez/kafka/connector/model/Label.java
@@ -1,8 +1,17 @@
 package com.github.felipegutierrez.kafka.connector.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
 public class Label {
 
     private final Map<String, Object> additionalProperties = new HashMap<String, Object>();
@@ -11,105 +20,4 @@ public class Label {
     private String name;
     private String color;
     private Boolean _default;
-
-    /**
-     * No args constructor for use in serialization
-     */
-    public Label() {
-    }
-
-    /**
-     * @param id
-     * @param color
-     * @param _default
-     * @param name
-     * @param url
-     */
-    public Label(Integer id, String url, String name, String color, Boolean _default) {
-        super();
-        this.id = id;
-        this.url = url;
-        this.name = name;
-        this.color = color;
-        this._default = _default;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Label withId(Integer id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Label withUrl(String url) {
-        this.url = url;
-        return this;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Label withName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public Label withColor(String color) {
-        this.color = color;
-        return this;
-    }
-
-    public Boolean getDefault() {
-        return _default;
-    }
-
-    public void setDefault(Boolean _default) {
-        this._default = _default;
-    }
-
-    public Label withDefault(Boolean _default) {
-        this._default = _default;
-        return this;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
-    public Label withAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-        return this;
-    }
-
 }
--- kafka-source-connector-github/src/main/java/com/github/felipegutierrez/kafka/connector/model/PullRequest.java
+++ kafka-source-connector-github/src/main/java/com/github/felipegutierrez/kafka/connector/model/PullRequest.java
@@ -1,5 +1,9 @@
 package com.github.felipegutierrez.kafka.connector.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -8,6 +12,10 @@ import java.util.Map;
 import static com.github.felipegutierrez.kafka.connector.schemas.GitHubSchemas.PR_HTML_URL_FIELD;
 import static com.github.felipegutierrez.kafka.connector.schemas.GitHubSchemas.PR_URL_FIELD;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
 public class PullRequest {
 
     private final Map<String, Object> additionalProperties = new HashMap<String, Object>();
@@ -16,95 +24,9 @@ public class PullRequest {
     private String diffUrl;
     private String patchUrl;
 
-    /**
-     * No args constructor for use in serialization
-     */
-    public PullRequest() {
-    }
-
-    /**
-     * @param diffUrl
-     * @param htmlUrl
-     * @param patchUrl
-     * @param url
-     */
-    public PullRequest(String url, String htmlUrl, String diffUrl, String patchUrl) {
-        super();
-        this.url = url;
-        this.htmlUrl = htmlUrl;
-        this.diffUrl = diffUrl;
-        this.patchUrl = patchUrl;
-    }
-
     public static PullRequest fromJson(JSONObject pull_request) {
         return new PullRequest()
-                .withUrl(pull_request.getString(PR_URL_FIELD))
-                .withHtmlUrl(pull_request.getString(PR_HTML_URL_FIELD));
-
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public PullRequest withUrl(String url) {
-        this.url = url;
-        return this;
-    }
-
-    public String getHtmlUrl() {
-        return htmlUrl;
-    }
-
-    public void setHtmlUrl(String htmlUrl) {
-        this.htmlUrl = htmlUrl;
-    }
-
-    public PullRequest withHtmlUrl(String htmlUrl) {
-        this.htmlUrl = htmlUrl;
-        return this;
-    }
-
-    public String getDiffUrl() {
-        return diffUrl;
-    }
-
-    public void setDiffUrl(String diffUrl) {
-        this.diffUrl = diffUrl;
-    }
-
-    public PullRequest withDiffUrl(String diffUrl) {
-        this.diffUrl = diffUrl;
-        return this;
-    }
-
-    public String getPatchUrl() {
-        return patchUrl;
-    }
-
-    public void setPatchUrl(String patchUrl) {
-        this.patchUrl = patchUrl;
-    }
-
-    public PullRequest withPatchUrl(String patchUrl) {
-        this.patchUrl = patchUrl;
-        return this;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
-    public PullRequest withAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-        return this;
+                .setUrl(pull_request.getString(PR_URL_FIELD))
+                .setHtmlUrl(pull_request.getString(PR_HTML_URL_FIELD));
     }
 }
